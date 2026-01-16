8dcea8d8f3f6b463aeeccc449bae72dbea4351f7
Mamadou Diallo <mamadou.9.diallo@student.vinci.be>
2026-01-16 16:41:47 +0100

    add all function emailValidator

--- emailValidator.ts
+++ emailValidator.ts
@@ -1,7 +1,33 @@
 export function isValidEmail(email: string): boolean {
-
+   
+    // c. Pas d'espace
     if (email.includes(" ")) {
     return false;
   }
+  
+  // a. Contient au moins un @
+  const atIndex = email.indexOf("@");
+  if (atIndex === -1) {
+    return false;
+  }
+
+   // d. texte avant et après @
+  const beforeAt = email.substring(0, atIndex);
+  const afterAt = email.substring(atIndex + 1);
+
+  if (beforeAt.length === 0 || afterAt.length === 0) {
+    return false;
+  }
+
+  // b. contient au moins un "." dans le nom de domaine
+  const dotIndex = afterAt.indexOf(".");
+  if (dotIndex === -1) {
+    return false;
+  }
+
+  if (dotIndex === afterAt.length - 1) {
+    return false;
+  }
+
   return true;
 }
