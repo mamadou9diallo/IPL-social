7110b9df155e0bd84d8a5041ae596a28bb1fc880
Mamadou Diallo <mamadou.9.diallo@student.vinci.be>
2026-01-16 17:01:56 +0100

    fix eslint typescript

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["node_modules/", "dist/"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {},
+  },
+];
